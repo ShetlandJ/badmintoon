d209d299f5abee621f61478a40f439ada46bbd9d
James <james@jastewart.co.uk>
2022-09-14 15:55:49 +0100

    Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,165 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { ref, computed } from "@vue/runtime-core";
+import Court from "./components/Court.vue";
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+const courts = ref([{ id: 1, players: [] }]);
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const addNewCourt = () => {
+    courts.value.push({ id: courts.value.length + 1, players: [] });
+};
+
+const players = ref(
+    [
+        "James B",
+        "Clara",
+        "James S",
+        "Ross Marnie",
+        "Alan M",
+        "Ross Mc",
+        "Callum",
+        "John",
+        "Alex",
+        "Gavin",
+        "Chris B",
+        "Connor B",
+        "Aaron",
+        "Joanna",
+        "Chris F",
+    ].map((player, index) => ({
+        name: player,
+        id: index,
+        attending: true,
+    }))
+);
+
+const attendingPlayerCount = computed(() => {
+    return attendingPlayers.value.length;
+});
+
+const attendingPlayers = computed(() => {
+    return players.value.filter((player) => player.attending);
+});
+
+const toggleAttending = (player) => {
+    const playerIndex = players.value.findIndex((p) => p.id === player.id);
+
+    players.value[playerIndex].attending =
+        !players.value[playerIndex].attending;
+};
+
+const unassignedPlayers = computed(() => {
+    let assignedIds = [];
+
+    courts.value.forEach((court) => {
+        court.players.forEach((player) => {
+            assignedIds.push(player.id);
+        });
+    });
+
+    return attendingPlayers.value.filter(
+        (player) => !assignedIds.includes(player.id)
+    );
+});
+
+const randomiseAttendees = () => {
+    courts.value.forEach((court) => {
+        court.players = [];
+
+        for (let i = 0; i < 4; i++) {
+            const randomPlayer =
+                unassignedPlayers.value[
+                    Math.floor(Math.random() * unassignedPlayers.value.length)
+                ];
+
+            if (randomPlayer) {
+                court.players.push(randomPlayer);
+            }
+        }
+    });
+};
+
+const resetCourts = () => {
+    courts.value.forEach((court) => {
+        court.players = [];
+    });
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <template>
+    <div class="flex p-4">
+        <div class="ml-2" style="width: 350px">
+            <p class="text-xl mb-4"><b>Player list:</b></p>
+            <div
+                v-for="player in players"
+                :key="`${player.id}-${
+                    player.attending ? 'attending' : 'not-attending'
+                }`"
+                class="text-lg flex"
+            >
+                <p v-if="player.attending">✅</p>
+                <p v-else>⛔️</p>
+                <label
+                    class="ml-4 mr-4 pr-4"
+                    @click="toggleAttending(player)"
+                    :for="player.id"
+                    >{{ player.name }}</label
+                >
+            </div>
+
+            <div class="flex justify-center mt-4">
+                <button
+                    @click="randomiseAttendees"
+                    class="
+                        px-8
+                        py-3
+                        text-white
+                        bg-blue-600
+                        rounded
+                        focus:outline-none
+                        disabled:opacity-25
+                    "
+                >
+                    Randomise
+                </button>
+            </div>
+            <div class="flex justify-center mt-4">
+                <button
+                    @click="resetCourts"
+                    class="
+                        px-8
+                        py-3
+                        text-white
+                        bg-red-600
+                        rounded
+                        focus:outline-none
+                        disabled:opacity-25
+                    "
+                >
+                    Reset courts
+                </button>
+            </div>
+        </div>
+        <div class="justify-center flex flex-grow bg-yellow-100 items-center">
+            <div>
+                <div
+                    v-for="court in courts"
+                    :key="`count-${court.id + 1}-${court.players.length}`"
+                >
+                    <Court
+                        class="m-3"
+                        :court-data="court"
+                        :key="`${JSON.stringify(court.players)}-${court.id}`"
+                    />
+                    <div
+                        v-if="court.id === courts.length"
+                        class="flex justify-center"
+                    >
+                        <button @click="addNewCourt">
+                            + Add another court
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Court.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { computed } from "@vue/runtime-core";
+
+const props = defineProps({
+    courtData: {
+        type: Object,
+        required: true,
+    },
+});
+
+const players = computed(() => props.courtData.players);
+
+const player1 = players.value.length ? players.value[0]?.name : "";
+const player2 = players.value.length > 1 ? players.value[1]?.name : "";
+const player3 = players.value.length > 2 ? players.value[2]?.name : "";
+const player4 = players.value.length > 3 ? players.value[3]?.name : "";
+</script>
+
+<template>
+    <div class="court">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div class="flex justify-center items-center text-xl">{{ player1 }}</div>
+        <div></div>
+        <div></div>
+        <div class="flex justify-center items-center text-xl">{{ player2 }}</div>
+        <div></div>
+        <div></div>
+        <div class="flex justify-center items-center text-xl">{{ player3 }}</div>
+        <div class="flex justify-center items-center text-xl">{{ player4 }}</div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<style scoped>
+.court {
+    display: grid;
+    grid-template-rows: 40px 120px 120px 40px;
+    grid-template-columns: 50px 200px 80px 80px 200px 50px;
+    border: solid 1px black;
+    justify-content: center;
+    align-content: center;
+}
+.court > div {
+    border: solid black;
+    border-width: 1px 0 0 1px;
+    transition: all 0.5s;
+}
+.court > div:nth-child(6),
+.court > div:nth-child(12),
+.court > div:nth-child(16),
+.court > div:nth-child(22) {
+    border-right-width: 1px;
+}
+.court > div:nth-last-child(-n + 6) {
+    border-bottom-width: 1px;
+}
+.court > div:nth-child(9),
+.court > div:nth-child(10) {
+    grid-row: 2/4;
+}
+.court > div:nth-child(9) {
+    grid-column: 3;
+}
+.court > div:nth-child(10) {
+    grid-column: 4;
+}
+</style>
